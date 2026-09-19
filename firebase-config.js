7e07ef4e7bb8af31e2ca6ae079c8d4944cff32b4
yeri099-oss <yeri099@gmail.com>
2026-09-19 16:20:21 +0900

    firebase-config.js 만들기

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// 꽃밥이야기 업무일지 (flowerrice) Firebase 설정
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDi5emj2AxYcLOslOWkdjSlN8oxZGNR1M0",
+  authDomain: "flowerrice.firebaseapp.com",
+  projectId: "flowerrice",
+  storageBucket: "flowerrice.firebasestorage.app",
+  messagingSenderId: "151465894530",
+  appId: "1:151465894530:web:e4286a07d0075138c7ca9b"
+};
